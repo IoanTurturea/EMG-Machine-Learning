b51ccdfbd9ba4c801ff8db19624a227ee0f1e1d8
Ioan Turturea <38984346+IoanTurturea@users.noreply.github.com>
2020-12-29 15:26:02 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -168,6 +168,98 @@ def prepare_xy_train_val_test():
     return x_train, y_train, x_val, y_val, x_test, y_test
 
 
+
+# extracts data from files of database
+# located in directory: "/home/ioan/Desktop/Database/"
+# makes a data augmentation by overlapping
+# with a window of size 50%
+# returns 6 tensorflow.data.Dataset objects:
+# x_train, y_train, x_val, y_val, x_test, y_test
+# Obs: val is the same with dev
+def prepare_xy_train_val_test_BETA():
+    # data base read and format:
+    x_train, y_train = [], []
+    x_test, y_test = [], []
+    x_val, y_val = [], []
+
+    path = "/home/ioan/Desktop/Database/"
+
+    # window size, choose it to be 50 samples
+    # because 50 & 200Hz(sampling rate) = 250ms
+    # a sample = one line from a (*,8) matrix
+    N = 50
+    overlap = 25
+
+    # there are 3 folders in Database: Train, Val, Test
+    folders = os.listdir(path)
+
+    # iterate in folders
+    for folder in folders:
+        data = os.path.join(path, folder)
+        files = os.listdir(data)
+
+        # iterate files in a folder
+        for file in files:
+            filepath = data + "/" + str(file)
+            signal = np.loadtxt(filepath)
+            label = int((str(file))[5:7])
+            # here was the problem:
+            # returns size = rows*columns. Is this desired?
+            # length = signal.size
+            # instead use "len" attribute
+            # N - overlap = step
+            for i in range(0, len(signal) - N + 1, N - overlap):
+                window = signal[i: (i + N)]
+
+                if folder == 'Train':
+                    x_train.append(window)
+                    y_train.append(label)
+                elif folder == 'Test':
+                    x_test.append(window)
+                    y_test.append(label)
+                elif folder == 'Val':
+                    x_val.append(window)
+                    y_val.append(label)
+
+    # old implementation (before dec. 4)
+    # train_dataset = tf.data.Dataset.from_tensor_slices((np.asanyarray(x_train), np.asanyarray(y_train)))
+    # val_dataset = tf.data.Dataset.from_tensor_slices((np.asanyarray(x_val), np.asanyarray(y_val)))
+    # test_dataset = tf.data.Dataset.from_tensor_slices((np.asanyarray(x_test), np.asanyarray(y_test)))
+
+    # 4 dec. update:
+    # left away the old return implementation
+    # because it looks easier to make
+    # one_hot encoding and batch from this function
+    # rather than breaking the already zipped dataset object in
+    # data and label at the function call
+    # Be aware of the one_hot and batch size!
+    # The lambda expression can not be compiled!
+
+    x_train = np.asanyarray(x_train)
+    y_train = np.asanyarray(y_train)
+    x_val = np.asanyarray(x_val)
+    y_val = np.asanyarray(y_val)
+    x_test = np.asanyarray(x_test)
+    y_test = np.asanyarray(y_test)
+
+    # reshape x_train
+    x_train = np.reshape(x_train, (44030, 400, 1))
+    print(x_train.shape)
+
+    x_train = tf.data.Dataset.from_tensor_slices(x_train)
+    y_train = keras.utils.to_categorical(y_train, 13)
+    y_train = tf.data.Dataset.from_tensor_slices(y_train)
+
+    x_val = tf.data.Dataset.from_tensor_slices(x_val)
+    y_val = keras.utils.to_categorical(y_val, 13)
+    y_val = tf.data.Dataset.from_tensor_slices(y_val)
+
+    x_test = tf.data.Dataset.from_tensor_slices(x_test)
+    y_test = keras.utils.to_categorical(y_test, 13)
+    y_test = tf.data.Dataset.from_tensor_slices(y_test)
+
+    return x_train, y_train, x_val, y_val, x_test, y_test
+
 # NN architecture, basic TF API
 # used dropout and first layer of batch normalization
 # to effectively scale the input data
