0c59e4647d747bc25413ef33c3c288d313481ddc
Ioan Turturea <38984346+IoanTurturea@users.noreply.github.com>
2020-12-29 07:59:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from tensorflow.python.keras.models import Model, load_model, save_model
 print("TF version:", tf.__version__)
 
 x_train, y_train, x_dev, y_dev, _, _ = prepare_xy_train_val_test()
-#train_dataset = tf.data.Dataset.zip((x_train, y_train))
+train_dataset = tf.data.Dataset.zip((x_train, y_train))
 
 input = Input((50, 8), name='input')
 hidden = Dense(64, activation='relu', name='hidden_1')(input)
@@ -25,20 +25,14 @@ model = Model(inputs=input, outputs=output)
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # model = load_model('1.h5')
-# y_train = keras.utils.to_categorical(y_train, 13)
-# y_dev = keras.utils.to_categorical(y_dev, 13)
 
-accuracy = model.fit(x_train, callbacks=[ModelCheckpoint('1.h5', save_best_only=True, verbose=1)],
+accuracy = model.fit(train_dataset, callbacks=[ModelCheckpoint('1.h5', save_best_only=True, verbose=1)],
                      batch_size=64, epochs=2048, verbose=2, validation_data=(x_dev, y_dev))
+
+#accuracy = model.fit(train_dataset, batch_size=64, epochs=2048, verbose=2, validation_data=(x_dev, y_dev))
 
 model.summary()
 
-
-# model.fit(train_dataset,
-#             batch_size=64,
-#             epochs=2048,
-#             verbose=1,
-#             validation_data=(x_dev, y_dev))
 # print(accuracy)
 # model.save('firstmodel256_128b.h5')
 # model.save('1.h5')
