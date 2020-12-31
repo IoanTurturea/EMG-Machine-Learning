6cb549e84ffdceef91f51717099b81eeea547c6d
Ioan Turturea <38984346+IoanTurturea@users.noreply.github.com>
2020-12-31 09:49:25 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,7 +3,6 @@ import os
 import csv
 import tensorflow as tf
 
-
 # extracts data from files of database
 # located in directory: "/home/ioan/Desktop/Database/"
 # makes a data augmentation by overlapping
@@ -12,14 +11,13 @@ import tensorflow as tf
 # train_dataset, val_dataset, test_dataset
 from tensorflow import keras
 
+path = "/home/ioan/Desktop/Database/"
 
 def prepare_train_val_test():
     # data base read and format:
     x_train, y_train = [], []
     x_test, y_test = [], []
     x_val, y_val = [], []
-
-    path = "/home/ioan/Desktop/Database/"
 
     # window size, choose it to be 50 samples
     # because 50 & 200Hz(sampling rate) = 250ms
@@ -39,7 +37,7 @@ def prepare_train_val_test():
         for file in files:
             filepath = data + "/" + str(file)
             signal = np.loadtxt(filepath)
-            label = (str(file))[5:7]
+            label = int((str(file))[5:7])
             # here was the problem:
             # returns size = rows*columns. Is this desired?
             # length = signal.size
@@ -58,31 +56,51 @@ def prepare_train_val_test():
                     x_val.append(window)
                     y_val.append(label)
 
-    # old implementation (before dec. 4)
-    # train_dataset = tf.data.Dataset.from_tensor_slices((np.asanyarray(x_train), np.asanyarray(y_train)))
-    # val_dataset = tf.data.Dataset.from_tensor_slices((np.asanyarray(x_val), np.asanyarray(y_val)))
-    # test_dataset = tf.data.Dataset.from_tensor_slices((np.asanyarray(x_test), np.asanyarray(y_test)))
-
     # 4 dec. update:
-    # left away the old return implementation
-    # because it looks easier to make
-    # one_hot encoding and batch from this function
-    # rather than breaking the already zipped dataset object in
-    # data and label at the function call
     # Be aware of the one_hot and batch size!
     # The lambda expression can not be compiled!
 
-    xtrain = tf.data.Dataset.from_tensor_slices(np.asanyarray(x_train))
-    ytrain = tf.data.Dataset.from_tensor_slices(np.asanyarray(y_train))  # .map(lambda z: tf.one_hot(z, 13))
-    train_dataset = tf.data.Dataset.zip((xtrain, ytrain)).repeat().batch(16)
+    x_train = np.asanyarray(x_train)
+    y_train = np.asanyarray(y_train)
+    x_val = np.asanyarray(x_val)
+    y_val = np.asanyarray(y_val)
+    x_test = np.asanyarray(x_test)
+    y_test = np.asanyarray(y_test)
 
-    xval = tf.data.Dataset.from_tensor_slices(np.asanyarray(x_val))
-    yval = tf.data.Dataset.from_tensor_slices(np.asanyarray(y_val))  # .map(lambda z: tf.one_hot(z, 13))
-    val_dataset = tf.data.Dataset.zip((xval, yval)).repeat().batch(16)
+    # reshape x_train
+    length = np.size(x_train)
+    x_train = np.reshape(x_train, (int(length / 400), 1, 400))
+    print(x_train.shape)
 
-    xtest = tf.data.Dataset.from_tensor_slices(np.asanyarray(x_test))
-    ytest = tf.data.Dataset.from_tensor_slices(np.asanyarray(y_test))  # .map(lambda z: tf.one_hot(z, 13))
-    test_dataset = tf.data.Dataset.zip((xtest, ytest)).repeat().batch(16)
+    # reshape y_train
+    y_train = np.reshape(y_train, (len(y_train), 1, 1))
+    print(y_train.shape)
+
+    # make one-hot encodings
+    y_train = keras.utils.to_categorical(y_train, 13)
+    y_val = keras.utils.to_categorical(y_val, 13)
+    y_test = keras.utils.to_categorical(y_test, 13)
+
+    # old implementation (before dec. 4)
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+    test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+
+    # 4 dec. update:
+    # Be aware of the one_hot and batch size!
+    # The lambda expression can not be compiled!
+
+    # xtrain = tf.data.Dataset.from_tensor_slices(np.asanyarray(x_train))
+    # ytrain = tf.data.Dataset.from_tensor_slices(np.asanyarray(y_train))  # .map(lambda z: tf.one_hot(z, 13))
+    # train_dataset = tf.data.Dataset.zip((xtrain, ytrain)).repeat().batch(16)
+    #
+    # xval = tf.data.Dataset.from_tensor_slices(np.asanyarray(x_val))
+    # yval = tf.data.Dataset.from_tensor_slices(np.asanyarray(y_val))  # .map(lambda z: tf.one_hot(z, 13))
+    # val_dataset = tf.data.Dataset.zip((xval, yval)).repeat().batch(16)
+    #
+    # xtest = tf.data.Dataset.from_tensor_slices(np.asanyarray(x_test))
+    # ytest = tf.data.Dataset.from_tensor_slices(np.asanyarray(y_test))  # .map(lambda z: tf.one_hot(z, 13))
+    # test_dataset = tf.data.Dataset.zip((xtest, ytest)).repeat().batch(16)
 
     return train_dataset, val_dataset, test_dataset
 
@@ -100,8 +118,6 @@ def prepare_xy_train_val_test():
     x_test, y_test = [], []
     x_val, y_val = [], []
 
-    path = "/home/ioan/Desktop/Database/"
-
     # window size, choose it to be 50 samples
     # because 50 & 200Hz(sampling rate) = 250ms
     # a sample = one line from a (*,8) matrix
@@ -138,102 +154,6 @@ def prepare_xy_train_val_test():
                 elif folder == 'Val':
                     x_val.append(window)
                     y_val.append(label)
-
-    # old implementation (before dec. 4)
-    # train_dataset = tf.data.Dataset.from_tensor_slices((np.asanyarray(x_train), np.asanyarray(y_train)))
-    # val_dataset = tf.data.Dataset.from_tensor_slices((np.asanyarray(x_val), np.asanyarray(y_val)))
-    # test_dataset = tf.data.Dataset.from_tensor_slices((np.asanyarray(x_test), np.asanyarray(y_test)))
-
-    # 4 dec. update:
-    # left away the old return implementation
-    # because it looks easier to make
-    # one_hot encoding and batch from this function
-    # rather than breaking the already zipped dataset object in
-    # data and label at the function call
-    # Be aware of the one_hot and batch size!
-    # The lambda expression can not be compiled!
-
-    x_train = tf.data.Dataset.from_tensor_slices(np.asanyarray(x_train))
-    y_train = keras.utils.to_categorical(y_train, 13)
-    y_train = tf.data.Dataset.from_tensor_slices(np.asanyarray(y_train))
-
-    x_val = tf.data.Dataset.from_tensor_slices(np.asanyarray(x_val))
-    y_val = keras.utils.to_categorical(y_val, 13)
-    y_val = tf.data.Dataset.from_tensor_slices(np.asanyarray(y_val))
-
-    x_test = tf.data.Dataset.from_tensor_slices(np.asanyarray(x_test))
-    y_test = keras.utils.to_categorical(y_test, 13)
-    y_test = tf.data.Dataset.from_tensor_slices(np.asanyarray(y_test))
-
-    return x_train, y_train, x_val, y_val, x_test, y_test
-
-
-
-# extracts data from files of database
-# located in directory: "/home/ioan/Desktop/Database/"
-# makes a data augmentation by overlapping
-# with a window of size 50%
-# returns 6 tensorflow.data.Dataset objects:
-# x_train, y_train, x_val, y_val, x_test, y_test
-# Obs: val is the same with dev
-def prepare_xy_train_val_test_BETA():
-    # data base read and format:
-    x_train, y_train = [], []
-    x_test, y_test = [], []
-    x_val, y_val = [], []
-
-    path = "/home/ioan/Desktop/Database/"
-
-    # window size, choose it to be 50 samples
-    # because 50 & 200Hz(sampling rate) = 250ms
-    # a sample = one line from a (*,8) matrix
-    N = 50
-    overlap = 25
-
-    # there are 3 folders in Database: Train, Val, Test
-    folders = os.listdir(path)
-
-    # iterate in folders
-    for folder in folders:
-        data = os.path.join(path, folder)
-        files = os.listdir(data)
-
-        # iterate files in a folder
-        for file in files:
-            filepath = data + "/" + str(file)
-            signal = np.loadtxt(filepath)
-            label = int((str(file))[5:7])
-            # here was the problem:
-            # returns size = rows*columns. Is this desired?
-            # length = signal.size
-            # instead use "len" attribute
-            # N - overlap = step
-            for i in range(0, len(signal) - N + 1, N - overlap):
-                window = signal[i: (i + N)]
-
-                if folder == 'Train':
-                    x_train.append(window)
-                    y_train.append(label)
-                elif folder == 'Test':
-                    x_test.append(window)
-                    y_test.append(label)
-                elif folder == 'Val':
-                    x_val.append(window)
-                    y_val.append(label)
-
-    # old implementation (before dec. 4)
-    # train_dataset = tf.data.Dataset.from_tensor_slices((np.asanyarray(x_train), np.asanyarray(y_train)))
-    # val_dataset = tf.data.Dataset.from_tensor_slices((np.asanyarray(x_val), np.asanyarray(y_val)))
-    # test_dataset = tf.data.Dataset.from_tensor_slices((np.asanyarray(x_test), np.asanyarray(y_test)))
-
-    # 4 dec. update:
-    # left away the old return implementation
-    # because it looks easier to make
-    # one_hot encoding and batch from this function
-    # rather than breaking the already zipped dataset object in
-    # data and label at the function call
-    # Be aware of the one_hot and batch size!
-    # The lambda expression can not be compiled!
 
     x_train = np.asanyarray(x_train)
     y_train = np.asanyarray(y_train)
@@ -242,9 +162,21 @@ def prepare_xy_train_val_test_BETA():
     x_test = np.asanyarray(x_test)
     y_test = np.asanyarray(y_test)
 
-    # reshape x_train
-    x_train = np.reshape(x_train, (44030, 400, 1))
+    # reshape x_train, x_val, x_test
+    x_train = np.reshape(x_train, (int(np.size(x_train) / 400), 1, 400))
     print(x_train.shape)
+    x_val = np.reshape(x_val, (int(np.size(x_val) / 400), 1, 400))
+    print(x_val.shape)
+    x_test = np.reshape(x_test, (int(np.size(x_test) / 400), 1, 400))
+    print(x_test.shape)
+
+    # reshape y_train, y_val, y_test
+    y_train = np.reshape(y_train, (len(y_train), 1, 1))
+    print(y_train.shape)
+    y_val = np.reshape(y_val, (len(y_val), 1, 1))
+    print(y_val.shape)
+    y_test = np.reshape(y_test, (len(y_test), 1, 1))
+    print(y_test.shape)
 
     x_train = tf.data.Dataset.from_tensor_slices(x_train)
     y_train = keras.utils.to_categorical(y_train, 13)
@@ -260,18 +192,86 @@ def prepare_xy_train_val_test_BETA():
 
     return x_train, y_train, x_val, y_val, x_test, y_test
 
-# NN architecture, basic TF API
-# used dropout and first layer of batch normalization
-# to effectively scale the input data
-def nn_model(in_data):
-    bn = tf.layers.batch_normalization(in_data)
-    # 50 units, not necessary the best
-    fc1 = tf.layers.dense(bn, 50)
-    fc2 = tf.layers.dense(fc1, 50)
-    fc2 = tf.layers.dropout(fc2)
-    # output layer, 13 gestures
-    fc3 = tf.layers.dense(fc2, 13)
-    return fc3
+
+# extracts data from files of database
+# located in directory: "/home/ioan/Desktop/Database/"
+# makes a data augmentation by overlapping
+# with a window of size 50%
+# returns 6 tensorflow.data.Dataset objects:
+# x_train, y_train, x_val, y_val, x_test, y_test
+# Obs: val is the same with dev
+def prepare_xy_train_val_test_asnumpyarrays():
+    # data base read and format:
+    x_train, y_train = [], []
+    x_test, y_test = [], []
+    x_val, y_val = [], []
+
+    # window size, choose it to be 50 samples
+    # because 50 & 200Hz(sampling rate) = 250ms
+    # a sample = one line from a (*,8) matrix
+    N = 50
+    overlap = 25
+
+    # there are 3 folders in Database: Train, Val, Test
+    folders = os.listdir(path)
+
+    # iterate in folders
+    for folder in folders:
+        data = os.path.join(path, folder)
+        files = os.listdir(data)
+
+        # iterate files in a folder
+        for file in files:
+            filepath = data + "/" + str(file)
+            signal = np.loadtxt(filepath)
+            label = int((str(file))[5:7])
+            # here was the problem:
+            # returns size = rows*columns. Is this desired?
+            # length = signal.size
+            # instead use "len" attribute
+            # N - overlap = step
+            for i in range(0, len(signal) - N + 1, N - overlap):
+                window = signal[i: (i + N)]
+
+                if folder == 'Train':
+                    x_train.append(window)
+                    y_train.append(label)
+                elif folder == 'Test':
+                    x_test.append(window)
+                    y_test.append(label)
+                elif folder == 'Val':
+                    x_val.append(window)
+                    y_val.append(label)
+
+    x_train = np.asanyarray(x_train)
+    y_train = np.asanyarray(y_train)
+    x_val = np.asanyarray(x_val)
+    y_val = np.asanyarray(y_val)
+    x_test = np.asanyarray(x_test)
+    y_test = np.asanyarray(y_test)
+
+    # reshape x_train, x_val, x_test
+    x_train = np.reshape(x_train, (int(np.size(x_train) / 400), 400))
+    print(x_train.shape)
+    x_val = np.reshape(x_val, (int(np.size(x_val) / 400), 400))
+    print(x_val.shape)
+    x_test = np.reshape(x_test, (int(np.size(x_test) / 400), 400))
+    print(x_test.shape)
+
+    # reshape y_train, y_val, y_test
+    y_train = np.reshape(y_train, (len(y_train), 1))
+    print(y_train.shape)
+    y_val = np.reshape(y_val, (len(y_val), 1))
+    print(y_val.shape)
+    y_test = np.reshape(y_test, (len(y_test), 1))
+    print(y_test.shape)
+
+    # make one hot encodings
+    y_train = keras.utils.to_categorical(y_train, 13)
+    y_val = keras.utils.to_categorical(y_val, 13)
+    y_test = keras.utils.to_categorical(y_test, 13)
+
+    return x_train, y_train, x_val, y_val, x_test, y_test
 
 
 # Dead code for test purpouse
@@ -306,8 +306,6 @@ def write_database_toCSV():
     x_train, y_train = [], []
     x_test, y_test = [], []
     x_val, y_val = [], []
-
-    path = "/home/ioan/Desktop/Database/"
 
     # window size, choose it to be 50 samples
     # because 50 & 200Hz(sampling rate) = 250ms
@@ -383,8 +381,6 @@ def write_database_toCSV():
     x_train, y_train = [], []
     x_test, y_test = [], []
     x_val, y_val = [], []
-
-    path = "/home/ioan/Desktop/Database/"
 
     # window size, choose it to be 50 samples
     # because 50 & 200Hz(sampling rate) = 250ms
