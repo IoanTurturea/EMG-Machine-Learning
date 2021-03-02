d54e5bf9d1fd35ce58422c79227c2b59f5b59ed3
Ioan Turturea <38984346+IoanTurturea@users.noreply.github.com>
2021-03-02 08:54:25 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -96,3 +96,76 @@ def prepare_xy_train_val_test_asnumpyarrays():
     y_test = keras.utils.to_categorical(y_test, 13)
 
     return x_train, y_train, x_val, y_val, x_test, y_test
+
+
+def prepare_train_val_test_asDatasets():
+    # data base read and format:
+    x_train, y_train = [], []
+    x_test, y_test = [], []
+    x_val, y_val = [], []
+
+    # window size, choose it to be 50 samples
+    # because 50 & 200Hz(sampling rate) = 250ms
+    # a sample = one line from a (*,8) matrix
+    N = 50
+    overlap = 40
+    hamming = np.hamming(N * 8)  # did not improve accuracy :(
+    # hamming = np.reshape(hamming, (N*8, )) # reshape both window if wanted, but does not make improvements
+
+    folders = os.listdir(path)  # 3 folders in Database: Train, Val, Test
+
+    # iterate in folders
+    for folder in folders:
+        data = os.path.join(path, folder)
+        files = os.listdir(data)
+
+        # iterate files in a folder
+        for file in files:
+            filepath = data + "/" + str(file)
+            signal = np.loadtxt(filepath)
+            label = int((str(file))[5:7])
+
+            step = N - overlap
+            real_max_value = len(signal) - overlap
+            max_value = int(real_max_value / step) * step  # multiples of overlap must fit signal length.
+
+            # Here was a minor bug: windows dimensions not equal to last window dimension for overlap != 25
+            for i in range(0, max_value, step):
+                window = signal[i: (i + N)]
+                window = np.reshape(window, (1, N * 8))
+                # window = np.reshape(window, (N*8, )) # reshape both hamming if wanted, but does not make improvements
+                # window *= hamming
+
+                if folder == 'Train':
+                    x_train.append(window)
+                    y_train.append(label)
+                elif folder == 'Test':
+                    x_test.append(window)
+                    y_test.append(label)
+                elif folder == 'Val':
+                    x_val.append(window)
+                    y_val.append(label)
+
+    x_train = np.asanyarray(x_train)
+    y_train = np.asanyarray(y_train)
+    x_val = np.asanyarray(x_val)
+    y_val = np.asanyarray(y_val)
+    x_test = np.asanyarray(x_test)
+    y_test = np.asanyarray(y_test)
+
+    # reshape y's:
+    y_train = np.reshape(y_train, (len(y_train), 1, 1))
+    y_val = np.reshape(y_train, (len(y_val), 1, 1))
+    y_test = np.reshape(y_train, (len(y_test), 1, 1))
+
+    # make one hot encodings
+    y_train = keras.utils.to_categorical(y_train, 13)
+    y_val = keras.utils.to_categorical(y_val, 13)
+    y_test = keras.utils.to_categorical(y_test, 13)
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+    test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+
+    return train_dataset, val_dataset, test_dataset
+
